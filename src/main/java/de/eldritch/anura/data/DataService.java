a30bcae75904dfeb2bad46970a6987b46dcf1d18
TurtleException <turtle.exception@eldritch.de>
2022-02-06 03:26:43 +0100

    Update DataService | I hate SQL & JDBC

--- src/main/java/de/eldritch/anura/data/DataService.java
+++ src/main/java/de/eldritch/anura/data/DataService.java
@@ -2,6 +2,17 @@ package de.eldritch.anura.data;
 
 import de.eldritch.anura.Anura;
 import de.eldritch.anura.Instance;
+import de.eldritch.anura.data.entities.Meme;
+import de.eldritch.anura.util.logging.NestedLogger;
+import de.eldritch.anura.util.text.Language;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * The database connection to MySQL. This class is only instantiated once by {@link Anura} and provides a connection for
@@ -9,5 +20,109 @@ import de.eldritch.anura.Instance;
  * <p>This class should <b>always</b> be thread-safe as multiple {@link Instance} objects may call it simultaneously.
  */
 public class DataService {
+    private final NestedLogger logger;
+    private final SQLConnector sqlConnector;
 
+    public DataService() throws IOException, SQLException, NullPointerException {
+        this.logger = new NestedLogger("DataService", Anura.singleton.getLogger());
+
+        sqlConnector = new SQLConnector(logger);
+    }
+
+    /* ---------- DATA ---------- */
+
+    /**
+     * Provides a specific {@link Meme} determined by its ID.
+     * @param id Unique ID of the meme.
+     * @return Meme object.
+     * @throws SQLException if an SQL error occurs.
+     * @throws NullPointerException if the underlying {@link ResultSet} is empty.
+     */
+    public synchronized Meme getMemeById(long id) throws SQLException, NullPointerException {
+        ResultSet resultSet = sqlConnector.executeQuery("SELECT * FROM memes WHERE id = " + id + " LIMIT 1");
+
+        // check ResultSet
+        if (!resultSet.next())
+            throw new NullPointerException("Could not match id");
+
+        try {
+            // fetch data
+            Language language  = Language.valueOf(resultSet.getString("language"));
+            String   url       = resultSet.getString("url");
+            long     timestamp = resultSet.getLong("timestamp");
+            long     authorID  = resultSet.getLong("author_id");
+            int      rating    = resultSet.getInt("rating");
+
+            return new Meme(id, timestamp, url, language, authorID, rating);
+        } catch (Exception e) {
+            throw new SQLException("Could not retrieve meme", e);
+        }
+    }
+
+    /**
+     * Provides a random {@link Meme} while possibly excluding multiple entries specified by ID.
+     * @param exclude Array of IDs to exclude.
+     * @return Meme object.
+     * @throws SQLException if an SQL error occurs.
+     * @throws NullPointerException if the underlying {@link ResultSet} is empty.
+     */
+    public synchronized Meme getMemeRandom(long[] exclude) throws SQLException, NullPointerException {
+        return getMemeById(generateID(exclude));
+    }
+
+    /**
+     * Provides a {@link List} of frequently used time-zones. If the SQL-Query fails to execute properly or returns
+     * empty a default list is returned.
+     * <p>"Frequently used" is defined as the top 25 results ordered by amount of uses. Results that only occur once are
+     * excluded as they would represent an exception.
+     * @return List of frequently used TZDB zone IDs.
+     */
+    public synchronized @NotNull List<String> getRecentTimeZones() {
+        String query = "SELECT `timezone` FROM (SELECT `timezone`, COUNT(`timezone`) AS `count` FROM customers GROUP BY `timezone`) AS `frequent` WHERE `count` > 1 ORDER BY `count` DESC LIMIT 25";
+
+        try {
+            ResultSet resultSet = sqlConnector.executeQuery(query);
+            List<String> frequent = new ArrayList<>();
+
+            // abort if query returns empty
+            if (!resultSet.next())
+                throw new NullPointerException();
+
+            // pass data to list
+            do {
+                frequent.add(resultSet.getString(1));
+            } while (resultSet.next());
+
+            return frequent;
+        } catch (Exception e) {
+            // return defaults
+            return List.of("UTC", "Europe/Berlin");
+        }
+    }
+
+    /* ------------------------- */
+
+    /**
+     * Queries the SQL database for a random meme entry ID while possibly excluding multiple entries specified by ID.
+     * @param exclude Array of IDs to exclude.
+     * @return ID of the entry that doesn't match any of the IDs in the given array.
+     * @throws SQLException if an SQL error occurs
+     * @throws NullPointerException if the {@link ResultSet} is empty.
+     */
+    private long generateID(long[] exclude) throws SQLException, NullPointerException {
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < exclude.length; i++) {
+            strBuilder.append(" id != ").append(exclude[i]);
+            if (i != exclude.length - 1)
+                strBuilder.append(" AND");
+        }
+
+        ResultSet resultSet = sqlConnector.executeQuery("SELECT id FROM memes WHERE " + strBuilder.toString() + " ORDER BY RAND() LIMIT 1");
+
+        // check ResultSet
+        if (!resultSet.next())
+            throw new NullPointerException("Could not find non-excluded id");
+
+        return resultSet.getLong("id");
+    }
 }
--- src/main/java/de/eldritch/anura/data/SQLConnector.java
+++ src/main/java/de/eldritch/anura/data/SQLConnector.java
@@ -1,13 +1,19 @@
 package de.eldritch.anura.data;
 
+import de.eldritch.anura.Anura;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-// TODO: update to fit new version
 class SQLConnector {
     private final Logger logger;
 
@@ -19,19 +25,22 @@ class SQLConnector {
     // table templates
     private static final String[] TEMPLATES = {
             /* All the memes... ALL OF THEM MUHAHAHAHAHA */
-            "`memes` ( `id` VARCHAR(36) NOT NULL , `source` TEXT NOT NULL , `language` TEXT NULL , `user` BIGINT NOT NULL , PRIMARY KEY (`id`))",
+            "`memes` ( `id` VARCHAR(36) NOT NULL , `timestamp` TIMESTAMP NOT NULL , `url` TEXT NOT NULL , `language` TEXT NULL , `author_id` BIGINT NOT NULL , PRIMARY KEY (`id`))",
 
             /* Member guilds with their associated bot user ID and the chosen language and time zone. */
-            "`guilds` ( `id` BIGINT NOT NULL , `bot_id` BIGINT NOT NULL , `language` TEXT NOT NULL , `modules_id` INT NOT NULL , PRIMARY KEY (`id`, `bot_id`))",
+            "`guilds` ( `id` BIGINT NOT NULL , `bot_id` BIGINT NOT NULL , `language` TEXT NOT NULL , `modules_id` INT NOT NULL , `timezone` TEXT NOT NULL , PRIMARY KEY (`id`, `bot_id`))",
 
             /* All module configurations (enabled / disabled) for each instance & guild. */
             "`modules` ( `id` SMALLINT NOT NULL AUTO_INCREMENT , `data` BIGINT NOT NULL , PRIMARY KEY (`id`))"
     };
 
-    public SQLConnector(Logger logger, ConfigSection config) throws SQLException, IllegalConfigException {
+    SQLConnector(Logger logger) throws NullPointerException, SQLException, IOException {
         this.logger = logger;
 
-        extractConfig(config);
+        // get config
+        Properties properties = new Properties();
+        properties.load(new FileReader(new File(Anura.singleton.getDirectory(), "mysql.properties")));
+        extractConfig(properties);
 
         buildConnection();
         buildTables();
@@ -40,39 +49,50 @@ class SQLConnector {
     /**
      * Checks whether the config is valid and attempts to get all the data from it that is needed later. If either
      * address, database or user are missing or the port is invalid the method will throw an exception.
-     * @param config {@link ConfigSection} to retrieve that data from.
-     * @throws IllegalConfigException if the config is null, one of the values is missing or invalid.
+     * @param config {@link Properties} to retrieve that data from.
+     * @throws NullPointerException if the config is null, one of the values is missing or invalid.
      */
-    private void extractConfig(ConfigSection config) throws IllegalConfigException {
+    private void extractConfig(Properties config) throws NullPointerException {
         if (config == null)
-            throw new IllegalConfigException("Config may not be null");
+            throw new NullPointerException("Config may not be null");
 
-        address  = config.getString("address");
-        database = config.getString("database");
-        password = config.getString("password");
-        user     = config.getString("user");
-        port     = config.getInt("port");
+        // assign values
+        address        = config.getProperty("address");
+        database       = config.getProperty("database");
+        password       = config.getProperty("password");
+        user           = config.getProperty("user");
+        String portStr = config.getProperty("port");
 
+        // check values
         if (address == null)
-            throw new IllegalConfigException("Address may not be null");
+            throw new NullPointerException("Address may not be null");
         if (database == null)
-            throw new IllegalConfigException("Database may not be null");
+            throw new NullPointerException("Database may not be null");
         if (user == null)
-            throw new IllegalConfigException("User may not be null");
+            throw new NullPointerException("User may not be null");
+
+        // parse port to integer
+        try {
+            port = Integer.parseInt(portStr);
+        } catch (ClassCastException e) {
+            throw new NullPointerException("Missing valid port (Could not convert '" + portStr + "' to an integer)");
+        }
+
+        // check port
         if (port <= 0 || port >= 65535)
-            throw new IllegalConfigException("Missing valid port");
+            throw new NullPointerException("Missing valid port");
     }
 
     /**
      * Attempts to get a {@link Connection} from the {@link DriverManager}. Address, port, database, user and password
      * should already have been extracted from config.
      * @throws SQLException if the connection fails to build or is not valid.
-     * @see SQLConnector#extractConfig(ConfigSection)
+     * @see SQLConnector#extractConfig(Properties)
      */
     private void buildConnection() throws SQLException {
         connection = DriverManager.getConnection("jdbc:mysql://" + address + ":" + port + "/" + database, user, password);
 
-        if (!connection.isValid(8))
+        if (!connection.isValid(4))
             throw new SQLException("Connection is invalid");
     }
 
@@ -91,11 +111,11 @@ class SQLConnector {
 
     /* ------------------------- */
 
-    public boolean execute(String statement) throws SQLException {
+    synchronized boolean execute(@NotNull String statement) throws SQLException {
         return connection.createStatement().execute(statement);
     }
 
-    public ResultSet executeQuery(String statement) throws SQLException {
+    synchronized @NotNull ResultSet executeQuery(@NotNull String statement) throws SQLException {
         return connection.createStatement().executeQuery(statement);
     }
 }
